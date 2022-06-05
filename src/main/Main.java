784bd43a9b0a36fd740a6032c675b9d3bdd7e5b2
Miguel Carvalho <miguelcarv00@gmail.com>
2022-06-05 15:55:40 +0100

    Removed the state changes from main
    Fiz merge a toa, tive de remover o que o bastista fez no main...

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,35 +14,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
 
-        if(args[0].equals("-d")){
-            //debug mode
-
-            State.timetodeal = new Timetodeal();
-            State.timetodecide = new Timetodecide();
-            State.timetocheck = new Timetocheck();
-            State.current = State.timetobet;
-
-            ReadFile.readCommandFile("/Users/JB/Desktop/cmd-file.txt");
-            List<FullCommand> fullCommandList2 = ReadFile.fullCommandList;
-
-            for (FullCommand command: fullCommandList2){
-                State.current.enter(command);
-                State.current.update(command);
-            }
-
-
-
-
-
-        }else if (args[0].equals("-s")){
-            //simulation mode
-
-        }else{
-
-            System.out.println("Invalid passing Arguments");
-            System.exit(0);
-        }
-
         ReadFile.readCardFile("/Users/miguelcarvalho/Desktop/card-file.txt");
         Deck deck = new Deck(ReadFile.deck);
 
