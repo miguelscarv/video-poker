e9413672353f9e6d344117c10503788aa734d459
Miguel Carvalho <miguelcarv00@gmail.com>
2022-06-05 16:14:43 +0100

    espaço de merda

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        
+
         Deck deck = new Deck(Reader.readCardFile("/Users/miguelcarvalho/Desktop/card-file.txt"));
 
         List<FullCommand> fullCommandList = Reader.readCommandFile("/Users/miguelcarvalho/Desktop/cmd-file.txt");
