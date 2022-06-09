c1bc3467354418562b29ed16f02a2ee29739484c
Miguel Carvalho <miguelcarv00@gmail.com>
2022-06-09 23:55:11 +0100

    removed jar

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 cd src/
 javac command/*.java player/*.java player/cards/*.java  main/*.java game/*.java game/match/*.java
 jar cmf manifest.txt videopoker.jar command/* player/* main/* game/*
+rm  command/*.class player/*.class player/cards/*.class  main/*.class game/*.class game/match/*.class
 cd ..
 
 echo ""
