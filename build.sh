a7c08f29f7b86c67b7773d2028839766496c3763
Miguel Carvalho <miguelcarv00@gmail.com>
2022-06-10 13:56:26 +0100

    separated build from run script

--- build.sh
+++ build.sh
@@ -6,20 +6,4 @@ jar cmf manifest.txt videopoker.jar command/* player/* main/* game/*
 rm  command/*.class player/*.class player/cards/*.class  main/*.class game/*.class game/match/*.class
 cd ..
 
-echo ""
 echo "Done building jar named videopoker.jar..."
-
-if [[ "$1" == "-d" ]];
-then 
-	echo "Running it in DEBUG mode..."
-	echo ""
-	java -jar src/videopoker.jar $1 $2 ./card-file.txt ./cmd-file.txt
-elif [[ "$1" == "-s" ]];
-then 
-	echo "Running it in SIMULATION mode"
-	echo""
-	java -jar src/videopoker.jar $1 $2 $3 $4
-else
-	echo "Commands do not match either options..."
-fi
-
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+if [[ "$1" == "-d" ]];
+then 
+
+	echo "Running it in DEBUG mode..."
+	echo ""
+	java -jar src/videopoker.jar $1 10000 ./card-file.txt ./cmd-file.txt
+
+elif [[ "$1" == "-s" ]];
+then 
+
+	read -p "Bet amount and number of deals: " amount ndeals
+	echo "Running it in SIMULATION mode..."
+	echo""
+	java -jar src/videopoker.jar $1 10000 $amount $ndeals
+
+else
+	echo "Commands do not match either options..."
+fi
+
