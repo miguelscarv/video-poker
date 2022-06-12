ab1d3ec40dc461bf725dd6c39bcdb764cfda78a0
Miguel Carvalho <miguelcarv00@gmail.com>
2022-06-12 03:51:39 +0100

    changed run

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-if [[ "$1" == "-d" ]];
+if [ "$1" == "-d" ];
 then 
 
 	echo "Running it in DEBUG mode..."
 	echo ""
 	java -jar src/videopoker.jar $1 10000 ./card-file.txt ./cmd-file.txt
 
-elif [[ "$1" == "-s" ]];
+elif [ "$1" == "-s" ];
 then 
 
 	read -p "Bet amount and number of deals: " amount ndeals
