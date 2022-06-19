d74f215ed9394e9a52f2a619d0782ffa2b4d56e0
BigDickBaby69 <filipeferraz2000@gmail.com>
2022-06-19 23:30:22 +0100

    java doc (match, reader, illegalexception)

--- src/game/match/IllegalException.java
+++ src/game/match/IllegalException.java
@@ -2,10 +2,17 @@ package game.match;
 
 public class IllegalException extends Exception{
 
+	/**
+	 * Exception thrown from introducing an invalid command
+	 * @param s Illegal command
+	 */
     public IllegalException(String s){
         super(s + ": illegal command");
     }
 
+    /**
+     * Exception thrown from betting an invalid amount
+     */
     public IllegalException(){
         super("b: illegal amount");
     }
--- src/game/match/Match.java
+++ src/game/match/Match.java
@@ -17,6 +17,12 @@ public class Match {
 
     private boolean isDebugMode;
 
+    /**
+     * Constructor to create a match
+     * @param player Player of the match
+     * @param deck Deck of the match
+     * @param isDebugMode Boolean that represents whether the match is in Debug or Simulation mode
+     */
     public Match(Player player, Deck deck, boolean isDebugMode){
 
         this.player = player;
@@ -31,6 +37,10 @@ public class Match {
         this.isDebugMode = isDebugMode;
     }
 
+    /**
+     * Method responsible for making a bet
+     * @param command Command that represents a bet (may have the amount to bet)
+     */
     public void bet(FullCommand command){
         try {
             this.currentState.bet(command);
@@ -39,6 +49,9 @@ public class Match {
         }
     }
 
+    /**
+     * Method responsible for executing a deal
+     */
     public void dealCards(){
         try {
             this.currentState.dealCards();
@@ -46,6 +59,11 @@ public class Match {
             System.out.println(i.getMessage());
         }
     }
+    
+    /**
+     * Method responsible for holding the chosen cards 
+     * @param command Command that has the indexes of the cards to hold
+     */
     public void holdCards(FullCommand command){
         try{
             this.currentState.holdCards(command);
@@ -53,6 +71,10 @@ public class Match {
             System.out.println(i.getMessage());
         }
     }
+    
+    /**
+     * Method that prints advice from following the strategy
+     */
     public void printAdvice() {
         try{
             this.currentState.printAdvice();
@@ -62,19 +84,56 @@ public class Match {
 
     }
 
+    /**
+     * Method that prints the available credit
+     */
     public void printCredit() { this.currentState.printCredit(); }
+    
+    /**
+     * Method that prints the statistics of the match
+     */
     public void printStatistics() { this.currentState.printStatistics(); }
 
+    /**
+     * Getter of the attribute canBet
+     * @return State in which it is possible to bet
+     */
     public State getCanBet() { return this.canBet; }
+    
+    /**
+     * Getter of the attribute canDealCards
+     * @return State in which it is possible to deal
+     */
     public State getCanDealCards() { return this.canDealCards; }
+    
+    /**
+     * Getter of the attribute canHoldCards
+     * @return State in which it is possible to hold hand cards
+     */
     public State getCanHoldCards() { return this.canHoldCards; }
 
+    /**
+     * Getter of the attribute player
+     * @return Player of the match
+     */
     public Player getPlayer() { return this.player; }
 
+    /**
+     * Getter of the attribute deck
+     * @return Deck of the match
+     */
     public Deck getDeck() { return this.deck; }
 
+    /**
+     * Getter of the attribute isDebugMode
+     * @return Whether the match is in debug or simulation mode
+     */
     public boolean getIsDebugMode() { return this.isDebugMode; }
 
+    /**
+     * Changes the state(bet, deal, hold) in which the match is
+     * @param s The new state
+     */
     public void setState(State s) { this.currentState = s; }
 
 
